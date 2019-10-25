82335c8212b93cc01b2c4867e8baf9f3a78ccb78
Jeff Beck <jbeck@airsquirrels.com>
2019-10-24 22:47:32 -0400

    Added connecting to the ec2 instance via ssh

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
+const Client = require('ssh2').Client;
+
 exports.handler = async (event, context) => {
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+    const conn = new Client();
+
+    // handle ready signal from SSH connection
+    conn.on('ready', function () {
+        console.log('SSH session open!');
+
+        // After ready signal execute the command to run the php application
+        conn.exec('php todoPhpLegacy/todo.php', (err, stream) => {
+            // handle errors
+            if (err) {
+                console.error('ERROR: ', err);
+            }
+            // watch for stream close
+            stream.on('close', (code, signal) => {
+                console.log('SSH session closed! ');
+                conn.end();
+            })
+                .on('data', (data) => {
+                    // probably going to want to do more than console log the legacy applications response
+                    console.log(data.toString());
+                })
+                // handle any errors written to the stderr
+                .stderr.on('data', function (data) {
+                    console.error('ERROR: ', data);
+                });
+        });
+    })
+    .connect({
+        host: 'ec2-54-91-241-131.compute-1.amazonaws.com',
+        port: 22,
+        username: 'ec2-user',
+        privateKey: require('fs').readFileSync('awsKey/ec2demo.pem')
+    });
     return;
 }
